f6cfb7ca23e5f3f65330fe48b0029779593a0723
Almog <alicranck@gmail.com>
2017-08-11 04:35:30 +0300

    first commit of featureExtractor. no loss and training loop

---
+++ featureExtractor.py
@@ -0,0 +1,74 @@
+from featureExtractorModule import *
+from featureExtractorUtils import *
+from imageReader import *
+import tensorflow.contrib.slim as slim
+from tensorflow.contrib.slim.nets import resnet_v2
+from models.slim.preprocessing import inception_preprocessing
+import matplotlib.pyplot as plt
+from imageReader import *
+import skimage.io as io
+import numpy as np
+#slim = tf.contrib.slim
+
+R_MEAN = 123.68
+G_MEAN = 116.78
+B_MEAN = 103.94
+
+
+# Path to checkpoint directory, for loading pre-trained resnet_101 weights (via tf.slim)
+checkpoints_dir = 'C:\Almog\MLProject/resnet_v2_101_2017_04_14/'
+
+# Paths to images directory (data_dir), and labels (labels_dir).
+data_dir = 'C:\Almog\MLProject\pascal voc\VOCtrainval_11-May-2012_2\JPEGImages/'
+labels_dir = 'C:\Almog\MLProject\pascal voc\VOCtrainval_11-May-2012_2\SegmentationObject/'
+
+# Path to a file containing names of all images and labels in directories (eg 2011_000036)
+data_list = 'C:\Almog\MLProject\pascal voc\VOCtrainval_11-May-2012_2\ImageSets\Segmentation/train.txt'
+
+mean = (R_MEAN, G_MEAN, B_MEAN)
+coord = tf.train.Coordinator()
+
+# Create an ImageReader instance to read images and labels from disk and turn into batches
+# more information in ImageReader.py
+reader = ImageReader(data_dir, labels_dir, data_list=data_list, input_size=(256,256), random_scale=False
+                     , random_mirror=False, ignore_label=0, img_mean=mean, coord=coord)
+
+# Collect a batch of 5 [img, label] pairs, from tensorflow queue defined by reader
+inputs = reader.dequeue(5)
+
+# ---debug print----
+print("inputs:")
+print(inputs)
+
+# Define network operation. network is defined in featureExtractorModule.py
+features, scaled_feature_map = extract_features(inputs[0], True)
+
+init = tf.global_variables_initializer()
+
+# Create an init function that loads pre-trained resnet_101 weights from checkpoint_dir
+init_fn = slim.assign_from_checkpoint_fn(
+            os.path.join(checkpoints_dir, 'resnet_v2_101.ckpt'),
+            slim.get_model_variables('resnet_v2_101'))
+
+# Training session
+with tf.Session() as sess:
+
+    sess.run(init)
+    init_fn(sess)
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    try:
+        while not coord.should_stop():
+            # Run training steps or whatever
+            # TODO - define loss and training op to run tf.train
+            inps = inputs[1].eval()
+            print(inps[1, 150, :, 0])
+    except tf.errors.OutOfRangeError:
+        print('Done training -- epoch limit reached')
+    finally:
+        # When done, ask the threads to stop.
+        coord.request_stop()
+
+    # Wait for threads to finish.
+    coord.join(threads)
+    sess.close()
---
+++ featureExtractorModule.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+import numpy as np
+import os
+from tensorflow.contrib.slim.nets import resnet_v2
+from tensorflow.contrib.slim.python.slim.nets import resnet_utils
+import skimage.io as io
+
+slim = tf.contrib.slim
+
+
+'''
+ Code for feature extracting network. this network takes a batch of images shaped (batch, h, w, 3) and outputs
+a (batch, h, w, 64) matrix with a 64d feature vector for each pixel in an input image. size is guaranteed to perfectly 
+match only if image dimensions are a power of 2 (256*256, 256*512 etc.)
+
+ The network is based on resnet_v2_101 model imported from tf.contrib.slim, and can optionally use weights 
+pre-trained on ImageNet.
+
+ Upscaling is done using transposed convolutions (tf.contrib.layers.conv2d_transpose) and each layer includes 
+batch normalization.
+'''
+def extract_features(inputs, is_training):
+
+    # TODO - Add skip connections between conv-deconv layers
+    with slim.arg_scope(resnet_utils.resnet_arg_scope(is_training=is_training)):
+
+        net, end_points = resnet_v2.resnet_v2_101(inputs,
+                                                None,
+                                                global_pool=False,
+                                                output_stride=16)
+
+        deconv1 = deconv_block(net,num_inputs=2048, num_outputs=1024,
+                               is_training=is_training, scope='deconv1')
+
+        deconv2 = deconv_block(deconv1, num_inputs=1024, num_outputs=512,
+                               stride=2, is_training=is_training, scope='deconv2')
+
+        deconv3 = deconv_block(deconv2, num_inputs=512, num_outputs=256,
+                               stride=2, is_training=is_training, scope='deconv3')
+
+        deconv4 = deconv_block(deconv3, num_inputs=256,num_outputs=128,
+                               stride=2, is_training=is_training, scope='deconv4')
+
+        deconv5 = deconv_block(deconv4, num_inputs=128,num_outputs=64,
+                               stride=2, is_training=is_training, scope='deconv5')
+
+    return deconv5, net
+
+
+def deconv_block(inputs, num_inputs, num_outputs, stride=1, kernel_size=3, is_training=False, scope=""):
+
+    with tf.variable_scope(scope+'_block'):
+        layer1 = tf.contrib.layers.conv2d_transpose(inputs, num_inputs, kernel_size, 1,
+                                                normalizer_fn=tf.contrib.layers.batch_norm,
+                                                trainable=is_training, scope=scope+'_1')
+
+        output = tf.contrib.layers.conv2d_transpose(tf.add(layer1, inputs), num_outputs, kernel_size, stride,
+                                                normalizer_fn=tf.contrib.layers.batch_norm,
+                                                trainable=is_training, scope=scope + '_2')
+
+    return output
+
+
+
+
+
